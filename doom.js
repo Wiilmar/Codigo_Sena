3b22410246c0b283783751859a3f7a571dff6ed7
Wilman <130717547+Wiilmar@users.noreply.github.com>
2023-06-09 14:27:37 -0500

    Nuevo cambio en el archivo doom

--- doom.js
+++ doom.js
@@ -1,8 +1,10 @@
-let recorrido;
+// let recorrido;
 // recorrido = document.head;
 // recorrido=document.forms[0];
 // recorrido = document.links;
 // recorrido = document.links[2];
-recorrido = document.images;
+// recorrido = document.images[1];
+// console.log(recorrido);
 
-console.log(recorrido);
+const encabezado = document.getElementsByClassName('contenedor');
+console.log(encabezado)
