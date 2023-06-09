c6ad858c8aef726cf5b4bf4b602c9526340c4b34
Wilman <130717547+Wiilmar@users.noreply.github.com>
2023-06-09 14:41:27 -0500

    Se movieron los archibos a una nueva carpeta

---
+++ Clase_doom/doom.js
@@ -0,0 +1,16 @@
+// let recorrido;
+// recorrido = document.head;
+// recorrido=document.forms[0];
+// recorrido = document.links;
+// recorrido = document.links[2];
+// recorrido = document.images[1];
+// console.log(recorrido);
+
+// const encabezado = document.getElementsByClassName('contenedor');
+// console.log(encabezado)
+
+// const carta1 = document.querySelector('.info');
+// const carta1 = document.querySelector('.hospedaje .card:nth-child(3)');
+// const carta1 = document.querySelector('.contenido-hero #formulario');
+const carta1 = document.querySelectorAll('.card')
+console.log(carta1)
