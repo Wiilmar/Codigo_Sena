f062e2e74249d6a136eee706c1e4b3203a661845
Wilman <130717547+Wiilmar@users.noreply.github.com>
2023-06-09 14:14:10 -0500

    Se añadio el doom

---
+++ doom.js
@@ -0,0 +1,8 @@
+let recorrido;
+// recorrido = document.head;
+// recorrido=document.forms[0];
+// recorrido = document.links;
+// recorrido = document.links[2];
+recorrido = document.images;
+
+console.log(recorrido);
