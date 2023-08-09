e0f4d2624c4658b11e9a9f9cf390b9112e2ba016
Wilman <130717547+Wiilmar@users.noreply.github.com>
2023-08-09 16:07:54 -0500

    Nueva carpeta añadida

---
+++ [Ins] Javier/[2] Twitter/Twitter.js
@@ -0,0 +1,12 @@
+ 
+document.addEventListener("DOMContentLoaded", function() {
+    // const boton_like = document.getElementById("boton_like");
+    // const resetear = document.getElementById("Resetear")
+    let valor_contador = 0;
+    function contador () {
+        
+    }
+    function reseteocontador() {
+        
+     }
+})
