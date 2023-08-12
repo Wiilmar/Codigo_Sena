aa9f17bd707324d8d9238e2e9c208e3a4ac2db43
wilman <130717547+Wiilmar@users.noreply.github.com>
2023-08-12 16:27:07 -0500

    Formulario 2 de ryder realizado

--- [Ins] Ryder/Formulario[2]/formulario2.js
+++ [Ins] Ryder/Formulario[2]/formulario2.js
@@ -1,6 +1,9 @@
-const contenedor = document.querySelector(".Contenedor_notas")
-const cajas = prompt("Cuantos inputs quieres?")
-const identificadores = []
+const contenedor = document.querySelector(".Contenedor_notas");
+const cajas = prompt("Cuantos inputs quieres?");
+const identificadores = [];
+const numeros_usuario = [];
+let notas_con_porcentaje = [];
+
 for (let i=1; i<=cajas; i++) {
     const texto = document.createElement("input");
     texto.type = "number"
@@ -18,24 +21,45 @@ for (let i=1; i<=cajas; i++) {
 }
 
 const boton2 = document.getElementById("Boton_1")
-const numeros_imput = [];
-boton2.addEventListener("click",()=> {
-    for (i=1; i<=cajas; i++) {
-        let valores = document.getElementById(`Nota${i}`);
-        let valor = parseFloat((valores.value))
-        numeros_imput.push(valor)
-    }
 
-    proceso()
-    console.log(numeros_imput)
+boton2.addEventListener("click",()=> {
+    valor_notas()
+    function valor_notas() {
+        numeros_usuario.splice(0,numeros_usuario.length)
+        for (i=1; i<=cajas; i++) {
+            let valores = document.getElementById(`Nota${i}`);
+            let valor = parseFloat((valores.value))
+            numeros_usuario.push(valor)
+        }
+        console.log('Notas registradas por el usuario:',numeros_usuario)
+        proceso_notas(porcentaje_notas)
+    }
+    
 })
-function proceso () {
+
+function proceso_notas (porcentaje_notas) {
     let total_suma = 0;
-    numeros_imput.forEach(x => {
-        total_suma = total_suma+x/numeros_imput.length
+    let promedio_notas = 0;
+    numeros_usuario.forEach(x => {
+        total_suma = total_suma+x;
+        promedio_notas = total_suma/numeros_usuario.length;
+    
     });
-    console.log(total_suma)
+    console.log(`Suma de notas: ${total_suma}`)
+    console.log(`Promedio de notas: ${promedio_notas}`)
+    porcentaje_notas()
 }
+
+function porcentaje_notas () {
+    numeros_usuario.forEach(s => {
+        let porcentaje = (s*30)/100;
+        notas_con_porcentaje.push(porcentaje);
+    })
+    console.table("El total del porcentaje de cada nota es:", notas_con_porcentaje)
+    notas_con_porcentaje.splice(0,notas_con_porcentaje.length)
+    console.log("------------------------------------"); 
+}
+
 
 
 //funciones por valores y referencias
