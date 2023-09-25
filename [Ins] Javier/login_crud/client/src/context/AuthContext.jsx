6ddd22bd378ff206e28fa0925490e071d54eb92d
wilman <130717547+Wiilmar@users.noreply.github.com>
2023-09-24 20:20:07 -0500

    Trabajo de javier inicio de sesion

---
+++ [Ins] Javier/login_crud/client/src/context/AuthContext.jsx
@@ -0,0 +1,75 @@
+import { createContext, useState, useContext, useEffect } from "react";
+import { registerRequest, loginRequest } from '../api/auth'
+import Cookies from 'js-cookie'
+
+export const AuthContext = createContext();
+
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error("useAuth must be used within a AuthProvider")
+  }
+  return context
+}
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [errors, setErros] = useState([])
+
+  const signup = async (user) => {
+    try {
+      const res = await registerRequest(user)
+      console.log(res.data)
+      setUser(res.data)
+      setIsAuthenticated(true)
+    } catch (error) {
+      // console.log(error.response)
+      setErros(error.response.data)
+    }
+  }
+
+  const signin = async (user) => {
+    try {
+      const res = await loginRequest(user)
+      console.log(res)
+      setIsAuthenticated(true)
+      setUser(res.data)
+    } catch (error) {
+      if(Array.isArray(error.response.data)) {
+        return setErros(error.response.data)
+      }
+      setErros([error.response.data.message])
+    }
+  }
+
+  useEffect(() => {
+    if(errors.length > 0) {
+      const timer = setTimeout(() => {
+        setErros([])
+      },5000)
+      return () => clearTimeout(timer)
+    } 
+  }, [errors])
+
+  useEffect(() => {
+    const cookies = Cookies.get()
+    if (cookies.token) {
+      console.log(cookies.token)
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider 
+    value={{
+      signup,
+      signin, 
+      user,
+      isAuthenticated,
+      errors
+      }}
+      >
+        {children}
+    </AuthContext.Provider>
+  )
+}
---
+++ [Ins] Javier/login_crud/src/schemas/auth.schema.js
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  username: z.string({
+    required_error: "Username is required",
+  }),
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email({
+      message: "Email is not valid",
+    }),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(6, {
+      message: "Password must be at least 6 characters",
+    }),
+});
+
+export const loginSchema = z.object({
+  email: z.string({
+    required_error: "Email is required",
+  }).email({
+      message: "Email is not valid"
+  }),
+  password: z.string({
+    required_error: "Password is required",
+  }).min(6, {
+    message: "Password must be at leat 6 characters",
+  })
+});
+
+//aqui 1:46:16
